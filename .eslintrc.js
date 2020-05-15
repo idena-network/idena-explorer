cf4127638f010083fa5ea47664c471b1805fdf48
ridenaio <r@idena.io>
2020-05-15 15:17:39 +0500

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react'],
+  extends: ['wesbos'],
+};
