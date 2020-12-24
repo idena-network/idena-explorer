55fe9c78b15f2b5fa2112e668cb80384c960a0d1
midenaio <m@idena.io>
2020-12-25 01:35:40 +0500

    Show simple criteria for the hard fork voting

--- screens/hardfork/components/hardforkvoting.js
+++ screens/hardfork/components/hardforkvoting.js
@@ -45,12 +45,12 @@ export default function HardForkVoting() {
 
           return (
             <div className="row" key={idx}>
-              <div className="col-12 col-sm-6">
+              <div className="col-12 col-sm-12">
                 <h1>Hard fork voting</h1>
                 <div className="card">
                   <div className="info_block">
                     <div className="row">
-                      <div className="col-12 col-sm-6 bordered-col">
+                      <div className="col-12 col-sm-4 bordered-col">
                         <h3 className="info_block__accent">
                           <span>
                             {upd.votes} (
@@ -68,7 +68,20 @@ export default function HardForkVoting() {
                         </TooltipText>
                       </div>
 
-                      <div className="col-12 col-sm-6 bordered-col">
+                      <div className="col-12 col-sm-4 bordered-col">
+                        <h3 className="info_block__accent">
+                          <span>{votesRequired}</span>
+                        </h3>
+                        <TooltipText
+                          className="control-label"
+                          data-toggle="tooltip"
+                          tooltip="Votes required to activate the hard fork"
+                        >
+                          Votes required
+                        </TooltipText>
+                      </div>
+
+                      <div className="col-12 col-sm-4 bordered-col">
                         <h3 className="info_block__accent">
                           <span>
                             {missingVotes} (
@@ -80,51 +93,9 @@ export default function HardForkVoting() {
                         <TooltipText
                           className="control-label"
                           data-toggle="tooltip"
-                          tooltip="Missing votes to activate the hard fork"
+                          tooltip="Votes left to activate the hard fork"
                         >
-                          Missing votes
-                        </TooltipText>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-sm-6">
-                <h1>Hard fork activation criteria</h1>
-
-                <div className="card">
-                  <div className="info_block">
-                    <div className="row">
-                      <div className="col-12 col-sm-6 bordered-col">
-                        <h3 className="info_block__accent">
-                          <span>
-                            {Math.round(state.online * 0.8)} out of{' '}
-                            {state.online}
-                          </span>
-                        </h3>
-                        <TooltipText
-                          className="control-label"
-                          data-toggle="tooltip"
-                          tooltip="Requires 80% of online nodes to activate the hard fork"
-                        >
-                          Online nodes required
-                        </TooltipText>
-                      </div>
-
-                      <div className="col-12 col-sm-6 bordered-col">
-                        <h3 className="info_block__accent">
-                          <span>
-                            {Math.round((state.total / 3) * 2)} out of{' '}
-                            {state.total}
-                          </span>
-                        </h3>
-                        <TooltipText
-                          className="control-label"
-                          data-toggle="tooltip"
-                          tooltip="Requires 2/3 of the total number of nodes to activate the hard fork"
-                        >
-                          Total nodes required
+                          Votes left
                         </TooltipText>
                       </div>
                     </div>
