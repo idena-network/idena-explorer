2c6ac5c048562c5185260ea08519a4a34d3ac159
midenaio <47352542+midenaio@users.noreply.github.com>
2021-05-12 00:40:14 +0500

    Show activated hard fork status (#77)
    Co-authored-by: unknown <midenaio>

--- screens/hardfork/components/hardforkvoting.js
+++ screens/hardfork/components/hardforkvoting.js
@@ -16,7 +16,10 @@ const initialState = {
   upgradeData: null,
 }
 
-export default function HardForkVoting({upgrade = 5}) {
+export default function HardForkVoting({
+  upgrade = 5,
+  lastActivatedUpgrade = 5,
+}) {
   const [state, setState] = useState(initialState)
 
   useEffect(() => {
@@ -45,17 +48,29 @@ export default function HardForkVoting({upgrade = 5}) {
     state && state.upgradeData && new Date(state.upgradeData.endActivationDate)
   const now = new Date()
   const status =
-    now < startDate ? 'pending' : now < endDate ? 'voting' : 'finished'
-  const name =
-    status === 'pending' || status === 'voting'
-      ? 'Pending hard fork'
-      : 'Finished hard fork'
+    now < startDate
+      ? 'Pending'
+      : upgrade === lastActivatedUpgrade
+      ? 'Activated'
+      : now < endDate
+      ? 'Voting'
+      : 'Finished'
 
+  const upd =
+    state.upgradeVoting &&
+    state.upgradeVoting.filter((u) => u.upgrade === upgrade)
+
+  const votes = upd ? upd.votes : null
+
+  const votesRequired = votes
+    ? Math.round(Math.max(state.online * 0.8, (state.total * 2) / 3))
+    : null
+  const missingVotes = status === 'Activated' ? 0 : votesRequired - votes
   return (
     <div>
       {state.upgradeData && (
         <>
-          <h1>{name}</h1>
+          <h1>{`Hard fork #${upgrade}: ${status.toLowerCase()}`}</h1>
 
           <section className="section section_details">
             <div className="card">
@@ -83,126 +98,112 @@ export default function HardForkVoting({upgrade = 5}) {
           </section>
         </>
       )}
-      {status === 'pending' && !state.upgradeVoting && (
+      {status === 'Pending' && !state.upgradeVoting && (
         <h3>Voting will start soon...</h3>
       )}
 
-      {state.upgradeVoting &&
-        state.upgradeVoting
-          .filter((upd) => upd.upgrade === upgrade)
-          .map((upd, idx) => {
-            const votesRequired = Math.round(
-              Math.max(state.online * 0.8, (state.total * 2) / 3)
-            )
-            const missingVotes = votesRequired - upd.votes
+      {votes && (
+        <div>
+          <div className="row">
+            <div className="col-12 col-sm-6">
+              <h3>Hard fork activation criteria</h3>
 
-            return (
-              <div key={idx}>
-                <div className="row">
-                  <div className="col-12 col-sm-6">
-                    <h3>Hard fork activation criteria</h3>
-
-                    <div className="card">
-                      <div className="info_block">
-                        <div className="row">
-                          <div className="col-12 col-sm-6 bordered-col">
-                            <h3 className="info_block__accent">
-                              <span>
-                                {Math.round(state.online * 0.8)} out of{' '}
-                                {state.online}
-                              </span>
-                            </h3>
-                            <TooltipText
-                              className="control-label"
-                              data-toggle="tooltip"
-                              tooltip="Requires 80% of online nodes to activate the hard fork (delegated nodes excluded)"
-                            >
-                              Online nodes required
-                            </TooltipText>
-                          </div>
-
-                          <div className="col-12 col-sm-6 bordered-col">
-                            <h3 className="info_block__accent">
-                              <span>
-                                {Math.round((state.total / 3) * 2)} out of{' '}
-                                {state.total}
-                              </span>
-                            </h3>
-                            <TooltipText
-                              className="control-label"
-                              data-toggle="tooltip"
-                              tooltip="Requires 2/3 of the total number of nodes to activate the hard fork (delegated nodes excluded)"
-                            >
-                              Total nodes required
-                            </TooltipText>
-                          </div>
-                        </div>
-                      </div>
+              <div className="card">
+                <div className="info_block">
+                  <div className="row">
+                    <div className="col-12 col-sm-6 bordered-col">
+                      <h3 className="info_block__accent">
+                        <span>
+                          {Math.round(state.online * 0.8)} out of {state.online}
+                        </span>
+                      </h3>
+                      <TooltipText
+                        className="control-label"
+                        data-toggle="tooltip"
+                        tooltip="Requires 80% of online nodes to activate the hard fork (delegated nodes excluded)"
+                      >
+                        Online nodes required
+                      </TooltipText>
                     </div>
-                  </div>
 
-                  <div className="col-12 col-sm-6">
-                    <h3>Votes</h3>
-                    <div className="card">
-                      <div className="info_block">
-                        <div className="row">
-                          <div className="col-12 col-sm-6 bordered-col">
-                            <h3 className="info_block__accent">
-                              <span>
-                                {upd.votes} (
-                                {votesRequired &&
-                                  Math.round((upd.votes / votesRequired) * 100)}
-                                %){' '}
-                              </span>
-                            </h3>
-                            <TooltipText
-                              className="control-label"
-                              data-toggle="tooltip"
-                              tooltip="Total number of nodes supporting upcoming changes"
-                            >
-                              Voted for the fork
-                            </TooltipText>
-                          </div>
-
-                          <div className="col-12 col-sm-6 bordered-col">
-                            <h3 className="info_block__accent">
-                              <span>
-                                {missingVotes} (
-                                {votesRequired &&
-                                  Math.round(
-                                    (missingVotes / votesRequired) * 100
-                                  )}
-                                %)
-                              </span>
-                            </h3>
-                            <TooltipText
-                              className="control-label"
-                              data-toggle="tooltip"
-                              tooltip="Missing votes to activate the hard fork"
-                            >
-                              Missing votes
-                            </TooltipText>
-                          </div>
-                        </div>
-                      </div>
+                    <div className="col-12 col-sm-6 bordered-col">
+                      <h3 className="info_block__accent">
+                        <span>
+                          {Math.round((state.total / 3) * 2)} out of{' '}
+                          {state.total}
+                        </span>
+                      </h3>
+                      <TooltipText
+                        className="control-label"
+                        data-toggle="tooltip"
+                        tooltip="Requires 2/3 of the total number of nodes to activate the hard fork (delegated nodes excluded)"
+                      >
+                        Total nodes required
+                      </TooltipText>
                     </div>
                   </div>
                 </div>
-                <br />
-                {upd.upgrade && (
-                  <div key={upd.upgrade}>
-                    <br />
-                    <h3>Voting progress</h3>
-                    <HardForkHistory
-                      upgrade={upd.upgrade}
-                      votesRequired={votesRequired}
-                    />
-                    <br />
-                  </div>
-                )}
               </div>
-            )
-          })}
+            </div>
+
+            <div className="col-12 col-sm-6">
+              <h3>Votes</h3>
+              <div className="card">
+                <div className="info_block">
+                  <div className="row">
+                    <div className="col-12 col-sm-6 bordered-col">
+                      <h3 className="info_block__accent">
+                        <span>
+                          {votes
+                            ? `${votes} (${
+                                votesRequired &&
+                                Math.round((votes / votesRequired) * 100)
+                              }%)`
+                            : status}
+                        </span>
+                      </h3>
+                      <TooltipText
+                        className="control-label"
+                        data-toggle="tooltip"
+                        tooltip="Total number of nodes supporting upcoming changes"
+                      >
+                        Voted for the fork
+                      </TooltipText>
+                    </div>
+
+                    <div className="col-12 col-sm-6 bordered-col">
+                      <h3 className="info_block__accent">
+                        <span>
+                          {missingVotes
+                            ? `${missingVotes} (${
+                                votesRequired &&
+                                Math.round((missingVotes / votesRequired) * 100)
+                              }%)`
+                            : '-'}
+                        </span>
+                      </h3>
+                      <TooltipText
+                        className="control-label"
+                        data-toggle="tooltip"
+                        tooltip="Missing votes to activate the hard fork"
+                      >
+                        Missing votes
+                      </TooltipText>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br />
+        </div>
+      )}
+      <div>
+        <br />
+        <h3>Voting progress</h3>
+        <HardForkHistory upgrade={upgrade} votesRequired={votesRequired} />
+        <br />
+      </div>
     </div>
   )
 }
