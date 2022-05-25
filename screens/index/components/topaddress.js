17b440ad6bc22269c710e91fb0d98d5aa002933b
mbidenaio <mb@idena.io>
2022-05-25 10:51:37 +0500

    Sort addresses by stake

--- screens/index/components/topaddress.js
+++ screens/index/components/topaddress.js
@@ -1,17 +1,25 @@
+/* eslint-disable react/prop-types */
 import Link from 'next/link'
 import {useInfiniteQuery} from 'react-query'
-import {precise2, dnaFmt} from '../../../shared/utils/utils'
-import {getBalances} from '../../../shared/api'
+import React, {useState} from 'react'
+import {styled} from '@stitches/react'
+import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu'
+import {blackA} from '@radix-ui/colors'
+import {CheckIcon, DotsVerticalIcon} from '@radix-ui/react-icons'
 import {SkeletonRows} from '../../../shared/components/skeleton'
+import {getBalances} from '../../../shared/api'
+import {precise2, dnaFmt} from '../../../shared/utils/utils'
 
 const LIMIT = 30
 
 export default function TopAddress({visible}) {
+  const [sortBy, setSortBy] = useState('balance')
+
   const fetchBalances = (_, continuationToken = null) =>
-    getBalances(LIMIT, continuationToken)
+    getBalances(sortBy, LIMIT, continuationToken)
 
   const {data, fetchMore, canFetchMore, status} = useInfiniteQuery(
-    visible && 'topaddress',
+    visible && 'topaddress' && sortBy,
     fetchBalances,
     {
       getFetchMore: (lastGroup) =>
@@ -26,13 +34,16 @@ export default function TopAddress({visible}) {
       <table className="table">
         <thead>
           <tr>
-            <th>Address</th>
-            <th>Balance</th>
-            <th>Stake</th>
+            <th style={{width: '55%'}}>Address</th>
+            <th style={{width: '20%'}}>Balance</th>
+            <th style={{width: '20%'}}>Stake</th>
+            <th style={{width: '5%'}}>
+              <SortByMenu value={sortBy} onChangeValue={setSortBy} />
+            </th>
           </tr>
         </thead>
         <tbody>
-          {!visible || (status === 'loading' && <SkeletonRows cols={3} />)}
+          {!visible || (status === 'loading' && <SkeletonRows cols={4} />)}
           {data.map(
             (page) =>
               page &&
@@ -57,6 +68,7 @@ export default function TopAddress({visible}) {
                   </td>
                   <td>{dnaFmt(precise2(item.balance), '')}</td>
                   <td>{dnaFmt(precise2(item.stake), '')}</td>
+                  <td />
                 </tr>
               ))
           )}
@@ -77,3 +89,119 @@ export default function TopAddress({visible}) {
     </div>
   )
 }
+
+const StyledContent = styled(DropdownMenuPrimitive.Content, {
+  minWidth: 160,
+  backgroundColor: 'white',
+  borderRadius: 6,
+  boxShadow:
+    '0px 10px 38px -10px rgba(22, 23, 24, 0.35), 0px 10px 20px -15px rgba(22, 23, 24, 0.2)',
+  '@media (prefers-reduced-motion: no-preference)': {
+    animationDuration: '400ms',
+    animationTimingFunction: 'cubic-bezier(0.16, 1, 0.3, 1)',
+    animationFillMode: 'forwards',
+    willChange: 'transform, opacity',
+  },
+})
+
+const itemStyles = {
+  all: 'unset',
+  fontSize: '0.875rem',
+  color: blackA.blackA12,
+  lineHeight: 1,
+  borderRadius: 3,
+  display: 'flex',
+  alignItems: 'center',
+  height: 25,
+  position: 'relative',
+  padding: '8px 0 8px 13px',
+  userSelect: 'none',
+  cursor: 'pointer',
+
+  '&:focus': {
+    backgroundColor: blackA.blackA3,
+  },
+}
+
+const StyledRadioItem = styled(DropdownMenuPrimitive.RadioItem, {
+  '&[data-state="checked"]': {
+    color: '#578fff',
+  },
+  ...itemStyles,
+})
+
+const StyledLabel = styled(DropdownMenuPrimitive.Label, {
+  padding: '8px 0 5px 13px',
+  fontSize: '0.875rem',
+  lineHeight: '25px',
+})
+
+const StyledSeparator = styled(DropdownMenuPrimitive.Separator, {
+  height: 1,
+  backgroundColor: blackA.blackA3,
+})
+
+const StyledItemIndicator = styled(DropdownMenuPrimitive.ItemIndicator, {
+  position: 'absolute',
+  left: 130,
+  width: 25,
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: '#578fff',
+})
+
+export const DropdownMenu = DropdownMenuPrimitive.Root
+export const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger
+export const DropdownMenuContent = StyledContent
+export const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup
+export const DropdownMenuRadioItem = StyledRadioItem
+export const DropdownMenuItemIndicator = StyledItemIndicator
+export const DropdownMenuLabel = StyledLabel
+export const DropdownMenuSeparator = StyledSeparator
+
+const Box = styled('div', {})
+
+const IconButton = styled('button', {
+  all: 'unset',
+  fontFamily: 'inherit',
+  borderRadius: '15%',
+  height: 25,
+  width: 19,
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  '&:hover': {backgroundColor: blackA.blackA3},
+  '&:focus': {boxShadow: `none`, outline: 'none'},
+})
+
+const SortByMenu = ({value, onChangeValue}) => (
+  <Box>
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger asChild>
+        <IconButton>
+          <DotsVerticalIcon />
+        </IconButton>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent sideOffset={5} align="end">
+        <DropdownMenuLabel>Sort by</DropdownMenuLabel>
+        <DropdownMenuRadioGroup value={value} onValueChange={onChangeValue}>
+          <DropdownMenuRadioItem value="balance">
+            Balance
+            <DropdownMenuItemIndicator>
+              <CheckIcon />
+            </DropdownMenuItemIndicator>
+          </DropdownMenuRadioItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioItem value="stake">
+            Stake
+            <DropdownMenuItemIndicator>
+              <CheckIcon />
+            </DropdownMenuItemIndicator>
+          </DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  </Box>
+)
