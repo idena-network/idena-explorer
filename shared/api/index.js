401782ab72cbcf7bb30bc3426d168b5a36934833
mbidenaio <mb@idena.io>
2022-06-04 22:56:15 +0500

    Switch to idena.io

--- shared/api/index.js
+++ shared/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_API_URL = 'https://api.idena.org/api'
+const BASE_API_URL = 'https://api.idena.io/api'
 
 const COINGECKO_API_URL = 'https://api.coingecko.com/api/v3'
 
