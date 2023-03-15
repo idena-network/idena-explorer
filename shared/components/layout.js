45b5aa2abfae268e3d0c1e15045751b1cb51d06c
ridenaio <r@idena.io>
2023-03-15 17:51:51 +0500

    Test

--- shared/components/layout.js
+++ shared/components/layout.js
@@ -189,6 +189,30 @@ function Layout({children, title = '', signinLoading = false}) {
         </div>
       </header>
       <main className="main">
+        <Link
+          href={`/signin?callback_url=${encodeURIComponent(
+            router.pathname === '/signin'
+              ? router.query.callback_url || '/'
+              : router.asPath
+          )}&attempt=${parseInt(router.query.attempt || 0) + 1}`}
+        >
+          <a className="btn btn-signin">
+            <img
+              alt="signin"
+              className={`icon icon-logo-white-small ${
+                signinLoading ? 'hidden' : ''
+              }`}
+              width="24px"
+            />
+            <div className={`spinner ${signinLoading ? '' : 'hidden'}`}>
+              <div className="small progress">
+                <div />
+              </div>
+            </div>
+
+            <span>Sign-in with Idena</span>
+          </a>
+        </Link>
         <Container>{children}</Container>
       </main>
     </>
